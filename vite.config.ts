61307d18dc979071cf03e15e76433a9b499eefa1
Leonardo Piusi <leopidev@gmail.com>
2022-01-19 17:12:35 -0300

    chore: create vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      atoms: path.resolve(__dirname, './src/components/atoms'),
+      molecules: path.resolve(__dirname, './src/components/molecules'),
+      organisms: path.resolve(__dirname, './src/components/organisms'),
+      templates: path.resolve(__dirname, './src/components/templates'),
+    },
+  },
 });
