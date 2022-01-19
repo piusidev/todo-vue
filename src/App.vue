5022312d7bdc49d20e4a7fc96262c838e16ccb10
Leonardo Piusi <leopidev@gmail.com>
2022-01-19 16:29:35 -0300

    fix: setting ts setup for components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup lang="ts">
 import HomeTemplate from './components/templates/Home/index.vue';
 
 export default {
--- src/components/templates/Home/index.vue
+++ src/components/templates/Home/index.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup lang="ts">
 import HelloWorld from '../../atoms/Hello/index.vue';
 
 export default {
